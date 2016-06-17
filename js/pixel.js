67b0f756183b5c3f90f29153276fd0c5e0d9ce80
Mattdre <olsen_matthew@yahoo.com>
2016-06-16 21:05:07 -1000

    working on getting the paint method working, works on click!

--- js/pixel.js
+++ js/pixel.js
@@ -1,14 +1,61 @@
+
 var mainDiv = document.getElementById('mainDiv');
-console.log("Yo");
-  drawDiv = document.createElement('div');
-      drawDiv.className = "draw";
-      mainDiv.appendChild(drawDiv);
 
-  for (var i = 1; i<=20006; i++){
-            pixelBox = document.createElement('div');
-            pixelBox.className = "pixelBox";
-            pixelBox.id = "pix"+i;
-            //pixelBox.innerHTML = 'Test';
-            mainDiv.appendChild(pixelBox);
-      }
+headDiv = document.createElement('div');
+headDiv.id = 'headDiv';
+mainDiv.appendChild(headDiv);
 
+headSpan1 = document.createElement('span');
+headSpan2 = document.createElement('span');
+headSpan1.id = 'headSpan1';
+headSpan2.id = 'headSpan2';
+headSpan1.innerHTML = 'SPAN1';
+headSpan2.innerHTML = 'SPAN2';
+headDiv.appendChild(headSpan1);
+headDiv.appendChild(headSpan2);
+
+paintToolBar = document.createElement('div');
+paintToolBar.id = 'paintToolBar';
+mainDiv.appendChild(paintToolBar);
+
+toolsDiv = document.createElement('div');
+colorDiv = document.createElement('div');
+toolsDiv.id = 'toolsDiv';
+colorDiv.id = 'colorDiv';
+toolsDiv.innerHTML = 'Tools';
+colorDiv.innerHTML = 'Colors';
+paintToolBar.appendChild(toolsDiv);
+paintToolBar.appendChild(colorDiv);
+
+
+
+drawDiv = document.createElement('div');
+drawDiv.id = "draw";
+mainDiv.appendChild(drawDiv);
+
+var pixelDiv;
+var pixelBox;
+
+function createEventListener(element){
+  element.addEventListener('click', function(){
+    console.log('hiii');
+    element.style.backgroundColor = 'red';
+  });
+}
+
+function createGrid(){
+  for (var i = 0; i < 40; i++){
+    pixelDiv = document.createElement('div');
+    pixelDiv.className = "pixelDiv";
+    pixelDiv.id = "pix" + i;
+    for (var j = 0; j < 40; j++) {
+      pixelBox = document.createElement('span');
+      pixelBox.className = "pixelBox";
+      pixelBox.id = "pix" + i + j;
+      createEventListener(pixelBox);
+      pixelDiv.appendChild(pixelBox);
+    }
+  drawDiv.appendChild(pixelDiv);
+  }
+}
+createGrid();
